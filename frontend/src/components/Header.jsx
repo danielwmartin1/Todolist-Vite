33eeab65fe6ae1d7707b4da0cfa8d3510e5d9cfc
Daniel Martin <danielwmartin1@gmail.com>
2025-02-12 10:48:35 -0500

    Update header component to include logo and enhance styling with flexbox layout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "../index.css";
+import logo from '../logo.svg';
 
 function Header() {
   return (
     <React.StrictMode>
       <header>
-        <h1 className="projectTitle">
-          React To-Do-List
-        </h1>
+          <h1 className="header">
+            <img className="header-img" src={logo} alt='React Logo' style={{height: '100px', width: '100px', }}/>
+            React To-Do-List
+          </h1>
       </header>
     </React.StrictMode>
   );
